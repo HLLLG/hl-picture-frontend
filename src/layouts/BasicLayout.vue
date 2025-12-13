bc85b1dba47f905fefa1e26a0ec7717812d8243e
HLLLG <2162803956@qq.com>
2025-12-13 22:02:55 +0800

    init project

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/HLLLG" target="_blank">程序员HL</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import GlobalHeader from '@/components/GlobalHeader.vue'
+
+export default defineComponent({
+  components: { GlobalHeader },
+})
+</script>
+
+<style scoped>
+#basicLayout .header {
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+  padding-inline: 20px;
+}
+
+#basicLayout .content {
+  padding: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 30px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
---
+++ src/request.ts
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import { message } from 'ant-design-vue'
+
+const myAxios = axios.create({
+  baseURL: 'http://localhost:8123',
+  timeout: 60000,
+  // 是否携带cookie
+  withCredentials: true,
+})
+
+// 全局请求拦截器
+axios.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    return config
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+  },
+)
+
+// 全局响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    const { data } = response
+    // 未登录
+    if (data.code === 40100) {
+      // 对于不是获取用户信息的请求，并且用户不在登录界面，则跳转到登录界面
+      if (
+        !response.request.responseURL.includes('user/get/login') &&
+        !window.location.pathname.includes('/user/login')
+      ) {
+        message.warning('请先登录')
+        window.location.href = '/user/login?redirect=' + window.location.pathname
+      }
+    }
+    return response
+  },
+  function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+  },
+)
+
+export default myAxios
