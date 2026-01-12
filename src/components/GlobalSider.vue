a3bbfe25d8cd041d3806cb1f2131894354e400f0
HLLLG <2162803956@qq.com>
2026-01-13 04:28:39 +0800

    feat:space-crud

---
+++ src/components/GlobalSider.vue
@@ -0,0 +1,162 @@
+<template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <router-link to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="logo" />
+            <div class="title">HL云图库</div>
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <!-- 用户信息栏 -->
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <a-space>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar"></a-avatar>
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="doUpdateUser">
+                    <UserOutlined />
+                    个人中心
+                  </a-menu-item>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+          <div v-else><a-button type="primary" href="/user/login">登录</a-button></div>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, h, ref } from 'vue'
+import { HomeOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons-vue'
+import { MenuProps, message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import { logoutUserUsingPost, updateUserUsingPost } from '@/api/userController.ts'
+
+const loginUserStore = useLoginUserStore()
+
+const originMenu = [
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/add_picture',
+    label: '创建图片',
+    title: '创建图片',
+  },
+  {
+    key: '/admin/pictureManage',
+    label: '图片管理',
+    title: '图片管理',
+  },
+  {
+    key: '/admin/spaceManage',
+    label: '空间管理',
+    title: '空间管理',
+  },
+  {
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理',
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'https://github.com/HLLLG', target: '_blank' }, 'HL程序员'),
+    title: 'HL程序员',
+  },
+]
+
+const router = useRouter()
+const current = ref<string[]>([])
+// 路由钩子，监听路由变化，高亮菜单项
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+
+//路由跳转事件
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key,
+  })
+}
+
+// 用户注销
+const doLogout = async () => {
+  const res = await logoutUserUsingPost()
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      username: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push({
+      path: '/user/login',
+    })
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
+// 过滤菜单
+const filterMenu = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    if (menu?.key?.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== 'admin') {
+        return false
+      }
+    }
+    return true
+  })
+}
+// 展示在菜单的路由数组
+const items = computed<MenuProps['items']>(() => filterMenu(originMenu))
+
+const doUpdateUser = () => {
+  router.push('/user/update')
+}
+</script>
+
+<style scoped>
+#globalHeader {
+  justify-content: center;
+}
+
+#globalHeader .title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 48px;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+</style>
---
+++ src/components/PictureList.vue
@@ -0,0 +1,182 @@
+<template>
+  <div id="homePage">
+    <div class="search-bar">
+      <!-- 搜索框 -->
+      <a-input-search
+        v-model:value="searchParams.searchText"
+        placeholder="从海量图片中搜索"
+        enter-button="搜索"
+        size="large"
+        @search="doSearch"
+      />
+    </div>
+    <!-- 分类和标签 -->
+    <a-tabs v-model:activeKey="selectCategory" @change="doSearch">
+      <a-tab-pane key="all" tab="全部"></a-tab-pane>
+      <a-tab-pane v-for="category in categoryList" :key="category" :tab="category"></a-tab-pane>
+    </a-tabs>
+    <div class="tag-bar">
+      <span style="margin-right: 8px">标签:</span>
+      <a-space :size="[0, 8]" wrap>
+        <a-checkable-tag
+          v-for="(tag, index) in tagList"
+          :key="tag"
+          v-model:checked="selectTagList[index]"
+          @change="doSearch"
+        >
+          {{ tag }}
+        </a-checkable-tag>
+      </a-space>
+    </div>
+    <!-- 图片列表 -->
+    <a-list
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
+      :data-source="dataList"
+      :pagination="pagination"
+      :loading="loading"
+    >
+      <template #renderItem="{ item: picture }">
+        <a-list-item style="padding: 0">
+          <a-card hoverable>
+            <!-- 单张图片 -->
+            <template #cover>
+              <img
+                :src="picture.thumbnailUrl ?? picture.url"
+                :alt="picture.name"
+                style="height: 180px; object-fit: cover"
+                @click="doClickPicture(picture)"
+              />
+            </template>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">
+                    {{ picture.category ?? '默认' }}
+                  </a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, reactive, ref } from 'vue'
+import {
+  listPictureTagCategoryUsingGet,
+  listPictureVoByPageUsingPost,
+} from '@/api/pictureController.ts'
+import { message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+
+// 图片数据列表
+const dataList = ref<API.PictureVO[]>([])
+// 图片数
+const total = ref(0)
+const loading = ref(true)
+
+const searchParams = reactive<API.PictureQueryRequest>({
+  current: 1,
+  pageSize: 12,
+  sortField: 'createTime',
+  sortOrder: 'descend',
+})
+
+// 分页参数
+const pagination = computed(() => {
+  return {
+    current: searchParams.current,
+    pageSize: searchParams.pageSize,
+    total: total.value,
+    onChange: (page: number, pageSize: number) => {
+      searchParams.current = page
+      searchParams.pageSize = pageSize
+      fetchData()
+    },
+  }
+})
+
+// 获取图片数据
+const fetchData = async () => {
+  loading.value = true
+  const params = {
+    ...searchParams,
+    tags: [],
+  }
+  if (selectCategory.value !== 'all') {
+    params.category = selectCategory.value
+  }
+  selectTagList.value.forEach((useTag, index) => {
+    if (useTag) {
+      params.tags.push(tagList.value[index])
+    }
+  })
+
+  const res = await listPictureVoByPageUsingPost(params)
+  if (res.data.data) {
+    dataList.value = res.data.data.records ?? []
+    total.value = res.data.data.total ?? 0
+  } else {
+    message.error('获取数据失败' + res.data.data.message)
+  }
+  loading.value = false
+}
+
+// 搜索
+const doSearch = async () => {
+  // 重置搜索条件
+  searchParams.current = 1
+  fetchData()
+}
+
+const categoryList = ref<string[]>([])
+const selectCategory = ref<string>()
+const tagList = ref<string[]>([])
+const selectTagList = ref<boolean[]>([])
+
+// 获取标签和分类选项
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    tagList.value = res.data.data.tagList
+    categoryList.value = res.data.data.categoryList
+  } else {
+    message.error('加载选项失败，' + res.data.message)
+  }
+}
+
+const router = useRouter()
+
+const doClickPicture = (picture: API.PictureVO) => {
+  router.push({
+    path: `/picture/${picture.id}`,
+  })
+}
+
+// 页面请求的时候加载一次
+onMounted(() => {
+  fetchData()
+  getTagCategoryOptions()
+})
+</script>
+
+<style scoped>
+#homePage {
+  margin-bottom: 16px;
+}
+
+#homePage .search-bar {
+  max-width: 480px;
+  margin: 0 auto 16px;
+}
+
+#homePage .tag-bar {
+  margin-bottom: 16px;
+}
+</style>
